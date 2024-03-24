1363e1216615690e6f9612b913abd2ca2a458c84
mfarel <mfarel@openpass.com.ar>
2024-03-24 00:06:46 -0300

    add catch and run prod

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,19 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const config = new DocumentBuilder()
-    .setTitle('Fydely')
-    .setDescription('Todos los servicios para el manejo de cupones')
-    .setVersion('1.0')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('docs', app, document);
-  app.enableCors();
-  await app.listen(process.env.PORT || 3000);
+  try {
+    const app = await NestFactory.create(AppModule);
+    const config = new DocumentBuilder()
+      .setTitle('Fydely')
+      .setDescription('Todos los servicios para el manejo de cupones')
+      .setVersion('1.0')
+      .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('docs', app, document);
+    app.enableCors();
+    await app.listen(process.env.PORT || 3000);
+  } catch (error) {
+    console.error('Error al iniciar la aplicación:', error);
+  }
 }
 bootstrap();
